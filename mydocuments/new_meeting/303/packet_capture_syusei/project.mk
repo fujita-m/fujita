7504f309322468d6e24bb36d9cc7518b43eeb89b
Masaki Fujita <fujita@swlab.cs.okayama-u.ac.jp>
2016-07-26 12:52:39 +0900

    Update home dir

---
+++ mydocuments/new_meeting/303/packet_capture_syusei/project.mk
@@ -0,0 +1,20 @@
+#####################################################################
+# Files in the project
+#####################################################################
+
+# if the main TeX file is ``myproject.tex'', set:
+#   PROJECT = myproject
+PROJECT = packet_capture_syusei
+PROJECT_BIB = mybibdata.bib
+
+# All figure files are extracted from the single PDF file: FIG_MASTER_FILE.
+# FIG_PAGES indicates which pages are extracted and used in the main TeX file.
+#
+# For example:
+FIG_MASTER_FILE = figs/fig.pdf
+FIG_PAGES     = 1
+# makes:
+#   figs/fig1.pdf ... figs/fig5.pdf from figs/fig.pdf
+#
+#FIG_MASTER_FILE = figs/figs.pdf
+#FIG_PAGES       = 1 2
